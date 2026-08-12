69f497b64440e53e17a460b9cc03cba2c9198012
sanikanalage <nalagesanika9691@gmail.com>
2026-08-12 18:33:38 +0530

    Delete Core Python/Assignments/Assignment11/q5.py

--- Core Python/Assignments/Assignment11/q5.py
+++
@@ -1,6 +0,0 @@
-#Que5. Python Program to sort a List According to the length of the elements within the list 
-
-li = ["apple", "cat", "banana", "hi", "mango"]
-print('Original List =',li)
-li.sort(key=len)
-print("Sorted List =", li)
