a8ec398f2791c78c3282f02919c0435d05169755
sanikanalage <nalagesanika9691@gmail.com>
2026-08-14 17:26:13 +0530

    Add files via upload
    Test 2

---
+++ Core Python/Tests/q4.py
@@ -0,0 +1,16 @@
+#Que4. Write a program to print pattern
+#  1 0 1 0 1
+#  0 1 0 1 0
+#  1 0 1 0 1
+#  0 1 0 1 0
+#  1 0 1 0 1
+
+for i in range(1,6):
+    for j in range(1,6):
+        if(i%2!=0 and j%2!=0):
+            print('1',end=' ')
+        elif(i%2==0 and j%2==0):
+            print('1',end=' ')
+        else:
+            print('0',end=' ')
+    print()
